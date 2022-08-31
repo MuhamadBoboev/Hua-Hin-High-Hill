e2a5be4fa6198318553847ea2ea4f75d71947a04
Muhammad <bmuhamad915@gmail.com>
2022-09-01 02:29:24 +0300

    relflesh

--- js/script.js
+++ js/script.js
@@ -1,17 +1,40 @@
-'use strict'
-const burger = document.querySelector('.burger');
+"use strict";
+const burger = document.querySelector(".burger");
 const body = document.body;
-burger.addEventListener('click', function() {
-  let mainNav = document.querySelector('.header__menu');
-  let burgerSpan = document.querySelector('.burger::after');
+burger.addEventListener("click", function () {
+  let mainNav = document.querySelector(".header__menu");
+  let burgerSpan = document.querySelector(".burger::after");
 
-  let header = document.querySelector('.header');
-  header.classList.toggle('active')
+  let header = document.querySelector(".header");
+  header.classList.toggle("active");
 
-  let menuBlack = document.querySelector('.main-block_container');
-  menuBlack.classList.toggle('main-block__active');
+  let menuBlack = document.querySelector(".main-block_container");
+  menuBlack.classList.toggle("main-block__active");
 
-  mainNav.classList.toggle('active');
-  burger.classList.toggle('active');
-  body.classList.toggle('lock');
-})
+  mainNav.classList.toggle("active");
+  burger.classList.toggle("active");
+  body.classList.toggle("lock");
+});
+/* ************************************* */
+const tabLinks = document.querySelectorAll(".tabs a");
+const tabPanels = document.querySelectorAll(".tabs-panel");
+
+for (let el of tabLinks) {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    document.querySelector(".tabs li.active").classList.remove("active");
+    document.querySelector(".tabs-panel.active").classList.remove("active");
+
+    const parentListItem = el.parentElement;
+    parentListItem.classList.add("active");
+    const index = [...parentListItem.parentElement.children].indexOf(
+      parentListItem
+    );
+
+    const panel = [...tabPanels].filter(
+      (el) => el.getAttribute("data-index") == index
+    );
+    panel[0].classList.add("active");
+  });
+}
--- js/swiper.js
+++ js/swiper.js
@@ -1,11 +1,26 @@
 "use strict";
-var galleryTop = new Swiper(".s-plans__tabs", {
+let galleryTop = new Swiper(".s-plans__tabs", {
   spaceBetween: 20,
   slidesPerView: 5,
   // navigation: {
   //   nextEl: ".swiper-button-next",
   //   prevEl: ".swiper-button-prev",
   // },
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+});
+let construction = new Swiper(".construction__swiper", {
+  spaceBetween: 20,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   observer: true,
   observeParents: true,
   observeSlideChildren: true,
